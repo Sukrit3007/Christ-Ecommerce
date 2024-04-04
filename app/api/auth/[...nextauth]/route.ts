6f986a25209ebc068d63c638aadbb03d01999589
Sukrit <sukrit3007@outlook.com>
2024-04-05 01:07:23 +0530

    Fixing nextauth (added provider in route.js)

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,13 @@
-import { authOptions } from './Options';
-import NextAuth  from 'next-auth';
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+          })
+    ]
+})
 
-export{handler as GET, handler as POST}
+export { handler as GET, handler as POST }
