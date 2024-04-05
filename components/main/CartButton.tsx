1dea2cca7ff7188f60ba15096dcc754521eb9e91
Sukrit <sukrit3007@outlook.com>
2024-04-05 21:33:46 +0530

    bug fixed and cart icon changed

--- components/main/CartButton.tsx
+++ components/main/CartButton.tsx
@@ -6,14 +6,14 @@ import {
     SheetTitle,
     SheetTrigger,
 } from "@/components/ui/sheet"
-import { ShoppingCart } from "lucide-react"
+import { ShoppingBag } from "lucide-react"
 
 export default function CartButton() {
     return (
         <Sheet>
             <SheetTrigger>
                 <div className='flex flex-row gap-2 border px-4 py-2 h-10 rounded-full bg-primary text-primary-foreground'>
-                    <ShoppingCart className='w-5 h-5' />
+                    <ShoppingBag className='w-5 h-5' />
                     1
                 </div>
             </SheetTrigger>
